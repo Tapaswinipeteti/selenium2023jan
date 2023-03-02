5d7e123f98b8ef6c15c43c020e7a8b7bd748a417
Laxmi Surya <Laxmi Surya@LAPTOP-SBNKPFD2>
2023-03-02 14:12:45 +0530

    PP1 push

---
+++ src/surya/Tappu.java
@@ -0,0 +1,19 @@
+package surya;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Tappu {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
